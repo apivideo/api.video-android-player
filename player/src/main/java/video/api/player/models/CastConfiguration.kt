2198a8dc94b0c28bbcb38edab25d5bfd9bb1913c
ThibaultBee <37510686+ThibaultBee@users.noreply.github.com>
2023-02-06 18:02:23 +0100

    index on feature/cast: fe4c700 feat(*): wip add support for Google Cast

---
+++ player/src/main/java/video/api/player/models/CastConfiguration.kt
@@ -0,0 +1,34 @@
+package video.api.player.models
+
+/**
+ * Description of the video to play.
+ *
+ * @param videoId the video ID of the video to play
+ * @param videoType the [VideoType] of the video to play. Only [VideoType.VOD] is supported.
+ * @param token private video token (only needed for private video, set to null otherwise)
+ */
+data class VideoOptions(
+    val videoId: String,
+    val videoType: VideoType,
+    val token: String? = null
+) {
+    private val baseUrl = "${videoType.baseUrl}/$videoId"
+    private val vodUrl = baseUrl + (token?.let { "/token/$it" } ?: "")
+    private val liveUrl = videoType.baseUrl + (token?.let { "/private/$it" } ?: "") + "/$videoId"
+
+    val hlsManifestUrl = if (videoType == VideoType.VOD) {
+        "$vodUrl/hls/manifest.m3u8"
+    } else {
+        "$liveUrl.m3u8"
+    }
+
+    val sessionTokenUrl = if (videoType == VideoType.VOD) {
+        "$vodUrl/session"
+    } else {
+        hlsManifestUrl // Temp: return the same url as hlsManifestUrl for live
+    }
+
+    val mp4Url = "$vodUrl/mp4/source.mp4"
+
+    val thumbnailUrl = "$vodUrl/thumbnail.jpg"
+}
